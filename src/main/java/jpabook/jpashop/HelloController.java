1072ce880b6f05ccb02195d3dfbbb3e936659f9a
SongMinJun <dkaskgkdua@gmail.com>
2020-12-23 23:33:36 +0900

    JPA와 DB설정, 동작확인 - P6Spy 라이브러리 사용(쿼리 로깅) - 테스트코드로 db 입출력 확인

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@EnableJpaRepositories
 public class HelloController {
     @GetMapping("/hello")
     public String hello(Model model) {
