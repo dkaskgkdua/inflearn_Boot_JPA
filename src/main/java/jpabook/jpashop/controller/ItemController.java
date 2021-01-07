df1917e5fefdabb4e29f5f69477b2a64185d4666
SongMinJun <dkaskgkdua@gmail.com>
2021-01-07 23:47:34 +0900

    변경 감지와 병합

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -59,16 +59,34 @@ public class ItemController {
         return "items/updateItemForm";
     }
     @PostMapping("/items/{itemId}/edit")
-    public String updateItemForm(@ModelAttribute("form") BookForm form) {
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+    public String updateItemForm(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        /**
+         * book 은 준영속 엔티티라고 볼 수 있음.
+         * 준영속 엔티티는 영속성 컨텍스트가더는 관리하지 않는 엔티티를 말한다.
+         * id가 있다는 것은 이미 db에 저장되었다는 뜻이다.
+         *
+         * 이렇게 임의로 만들어낸 엔티티도 기존 식별자가 있으면 준영속 엔티티이다.
+         * 준영속 엔티티를 수정하는 방법은
+         * 1. 변경 감지 기능 사용
+         * 2. 병합 사용용         */
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
 
-        itemService.saveItem(book);
+        /**
+         *  위 코드는 좋은 설계가 아님
+         *  웹 계층에서만 사용할 form class가 service 로 넘어가는 건 좋은 방법이 아님
+         *  파라미터 단위로 넘기거나 좀 많으면 서비스계층에 DTO를 하나 만들어서 사용
+         *
+         *  ** 또한 컨트롤러에서 어설프게 엔티티를 생성안하는게 좋다.
+         *  ** setter는 안쓰고 뭔가 목적이 있는 메소드를 만들어서 역추적이 가능하게 해야함
+         */
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,32 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * !!...데이터를 수정하는 방법...!!
+     *
+     * 변경감지 기능 사용
+     * repo 에서 update를 따로 호출 안해도 저장이 됨.
+     * 이유는 영속성 컨텍스트에서 엔티티를 다시 조회 한 후에 데이터를 수정하게 되면
+     * 트랜잭션 커밋 시점에 변경 감지(Dirty Checking)이 동작해서 UPDATE 쿼리가 실행됨
+     *
+     * merge의 경우 jpa에서 아래와 같은 코드 기능을 해주는 것이다.
+     * ex) Item mergeItem = em.merge(item);
+     * merge는 준영속 상태의 엔티티를 영속 상태로 변경할 때 사용하는 기능이다.
+     * 그리고 그 엔티티를 반환한다. 그리고 그 엔티티는 영속성 컨텍스트에서 관리한다.
+     * 이후 뭔가 작업하려면 반환된 값을 사용해야 한다는 말이다.
+     *
+     * 주의!!
+     * 변경 감지 기능을 사용하면 원하는 속성만 선택해서 변경가능하지만, 병합을 사용하면
+     * 모든 속성이 변경된다. 병합시 값이 없으면 'null'로 업데이트될 위험도 있음..
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
